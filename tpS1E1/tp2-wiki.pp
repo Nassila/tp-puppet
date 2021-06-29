3af1c72f8be5bd0e012c310b60b369b7071136a5
root <root@server0>
2021-06-29 13:49:13 +0000

    wip

--- tpS1E1/tp2-wiki.pp
+++ tpS1E1/tp2-wiki.pp
@@ -1,8 +1,8 @@
-$path1 = '/usr/src'
-$path2 = '/usr/bin'
-$path3 = '/var/www'
-
 class dokuwiki {
+    $source_path = '/usr/src'
+    $binary_path = '/usr/bin'
+    $web_path = '/var/www'
+
     package { 'apache2':
             ensure => present
     }
@@ -16,23 +16,23 @@ class dokuwiki {
     file { 'download-dokuwiki':
             ensure => present,
             source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
-            path   => "${path1}/dokuwiki.tgz"
+            path   => "${source_path}/dokuwiki.tgz"
     }
 
 #etape 4 extraire l'archive dokuwiki.tgz
 
     exec { 'extract-dokuwiki':
             command => 'tar xavf dokuwiki.tgz',
-            cwd     => "${path1}",
-            path    => ["${path2}"],
+            cwd     => "${source_path}",
+            path    => ["${binary_path}"],
             require => File['download-dokuwiki'],
-            unless  => "test -d ${path1}/dokuwiki-2020-07-29"
+            unless  => "test -d ${source_path}/dokuwiki-2020-07-29"
     }
 
     file { 'rename-dokuwiki-2020-07-29':
             ensure  => present,
-            source  => "${path1}/dokuwiki-2020-07-29",
-            path    => "${path1}/dokuwiki",
+            source  => "${source_path}/dokuwiki-2020-07-29",
+            path    => "${source_path}/dokuwiki",
             require => Exec['extract-dokuwiki']
     }
 }
@@ -42,10 +42,10 @@ class dokuwiki {
 ##  Créer un dossier pour les données du site XXX
 
 class dokuwiki_deploy {
-    file { "create new directory for ${env}.wiki in ${path3} and allow apache to write in":            
+    file { "create new directory for ${env}.wiki in ${web_path} and allow apache to write in":            
             ensure  => directory,
-            source  => "${path1}/dokuwiki",
-            path    => "${path3}/${env}.wiki",
+            source  => "${source_path}/dokuwiki",
+            path    => "${web_path}/${env}.wiki",
             recurse => true,
             owner   => 'www-data',
             group   => 'www-data',
